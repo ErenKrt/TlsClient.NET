b9b64a0d354ef22d271d5986445fafb7452dc66d
Eren <kurteren07@gmail.com>
2026-09-06 02:53:33 +0300

    Updated for "v1.16.0" tls client

--- tests/TlsClient.Api.Tests/BuilderTests.cs
+++ tests/TlsClient.Api.Tests/BuilderTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using System.Net;
 using TlsClient.Core.Builders;
+using TlsClient.Core.Helpers;
 using TlsClient.Core.Models.Entities;
 using TlsClient.Core.Models.Requests;
 using TlsClient.Api.Extensions;
@@ -34,6 +35,45 @@ namespace TlsClient.Api.Tests
             response.Status.Should().Be(HttpStatusCode.OK);
             response.Body.Should().Contain("TlsClient.NET 1.0");
             response.Body.Should().Contain("must go on");
+        }
+
+        [Fact]
+        public void Should_Configure_Disable_Session_Tickets()
+        {
+            var tlsClient = new TlsClientBuilder()
+                .WithIdentifier(TlsClientIdentifier.Chrome133)
+                .WithDisableSessionTickets()
+                .WithApi(new Uri("http://127.0.0.1:8080"), "my-auth-key-1")
+                .Build();
+
+            var disabledRequest = new RequestBuilder()
+                .WithDisableSessionTickets()
+                .Build();
+            var enabledRequest = new RequestBuilder()
+                .WithDisableSessionTickets(false)
+                .Build();
+
+            tlsClient.Options.DisableSessionTickets.Should().BeTrue();
+            disabledRequest.DisableSessionTickets.Should().BeTrue();
+            enabledRequest.DisableSessionTickets.Should().BeFalse();
+
+            tlsClient.HttpClient.Dispose();
+        }
+
+        [Fact]
+        public void Should_Serialize_Trust_Anchors_Payload()
+        {
+            var request = new Request()
+            {
+                CustomTlsClient = new CustomTlsClient
+                {
+                    TrustAnchorsPayload = "test-trust-anchors"
+                }
+            };
+
+            var payload = request.ToJson();
+
+            payload.Should().Contain("\"trustAnchorsPayload\": \"test-trust-anchors\"");
         }
     }
 }
